d08cd9d34d6019648e490454b71c9ca3e76608ac
Matt Smith <msmith@apaxsoftware.com>
2016-08-16 10:26:26 -0400

    Add prompts for selecting a pizza to order

--- main.py
+++ main.py
@@ -1,14 +1,46 @@
+order = None
+
+def add_to_order():
+    """
+    Prompts for adding pizza to an order.
+    """
+    
+    ORDER_OPTIONS = (
+        "1: Cheese pizza",
+        "2: Pepperoni pizza",
+        "0: Back to main menu",
+    )
+    
+    while True:
+        for order_option in ORDER_OPTIONS:
+            print(order_option)
+
+        order_selection = input("Which pizza would you like to order? ")
+
+        if order_selection == "0":
+            break
+
+
+
 def main():
     """
     Main loop for the ordering application
     """
+    MENU_ITEMS = (
+        "1: Order Pizza",
+        "0: Exit",
+    )
 
     while(True):
-        print("0: Exit")
+        for menu_item in MENU_ITEMS:
+            print(menu_item)
+
         menu_selection = input("Please select an option from above? ")
 
         if menu_selection == "0":
             break
+        elif menu_selection == "1":
+            add_to_order()
 
 
 main()
