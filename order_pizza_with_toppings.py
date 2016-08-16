5bdd7ddee0760c48485b23f1b48ec9c5196a3156
Matt Smith <msmith@apaxsoftware.com>
2016-08-16 13:27:14 -0400

    Display cart menu

--- order_pizza_with_toppings.py
+++ order_pizza_with_toppings.py
@@ -1,9 +1,4 @@
 
-class Cart():
-    """Cart is the shopping cart for the current user's order"""
-    def __init__(self):
-        super(Cart, self).__init__()
-
 class Pizza():
     """Pizza calculates price and manages toppings"""
     def __init__(self):
@@ -14,15 +9,42 @@ class Topping():
     def __init__(self):
         super(Topping, self).__init__()
 
+class Cart():
+    """Cart is the shopping cart for the current user's order"""
+    MENU_ITEMS = (
+        "1: Add Toppings",
+        "2: Remove Toppings",
+        "3: Add Pizza to Cart",
+        "0: Cancel",
+    )
+
+    current_pizza = Pizza()
+
+    def __init__(self):
+        super(Cart, self).__init__()
+
+    def display_menu(self):
+        while(True):
+            print("\n\n")
+            for menu_item in self.MENU_ITEMS:
+                print(menu_item)
+
+            menu_selection = input("\nPlease select an option from above? ")
+
+            if menu_selection == "0":
+                break
+
 def main():
     """
     Main loop for the ordering application
     """
     MENU_ITEMS = (
+        "1: Add Pizza",
+        "2: Remove Pizza",
         "0: Exit",
     )
 
-    order = []
+    cart = Cart()
 
     while(True):
         print("\n\n")
@@ -33,6 +55,8 @@ def main():
 
         if menu_selection == "0":
             break
+        if menu_selection == "1":
+            cart.display_menu()
 
 
 main()
